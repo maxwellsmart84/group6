720f89ae21a2404c4f60f63b2f6dc252793c2d20
Lindsay Williams <williamsl25@yahoo.com>
2015-11-05 20:37:35 -0500

    added lindsay.js.  need to check with team about naming

--- client/lindsay.js
+++ client/lindsay.js
@@ -8,22 +8,82 @@ var page= {
   },
   initStyling:function(){
     page.getUser();
+    page.grabActivityFromServer();
 
   },
   initEvents:function(){
 
-    getUser: function(){
+    },
+  getTemplate: function(name) {
+    return _.template(templates[name]);
+  },
+  loadTemplate: function(name, val) {
+      var tmpl = page.getTemplate(name);
+      return tmpl(val);
+  },
+  getUser: function(){
       $.ajax({
         url: '/getUser',
         method: 'GET',
         success: function(data){
           userData = JSON.parse(data);
-          page.loadTmple($(''))
+          page.loadTemplate($(''), userData, $('#userTmpl').html());
 
         }
-      })
+      });
+    },
+
+  loadActivity : function(arr){
+    _.each(arr, function(currVal, idx, arr){
+    //maintains the same ID regardless of view
+      if(arr === page.activityList){
+      currVal.id = idx;
+    }
+    else{
+      currVal.id = _.filter(page.activityList, function(obj){
+        return obj.item === currVal.item;})[0].id;
     }
 
+      if(currVal.status ==="Active"){
+      page.loadTemplate($('.activity-section'), currVal, $('#activityTmpl').html());
+    }
+    else{
+        toDo.loadTemplate($('.activity-section'), currVal, $('#completeActivityTmpl').html());
+
+        }
+      });
+    },
+
+  grabActivityFromServer: function() {
+    $.ajax({
+      type: 'GET',
+      url: '/getActivity',
+      success: function(data) {
+        console.log("SUCCESS: ", data);
+        activityData = JSON.parse(data);
+        page.loadActivity(data);
+      },
+      failure: function(data) {
+        console.log("FAILURE: ", data);
+      }
+    });
   },
+  sendActivityToServer: function(activity) {
+    console.log("IN TRANSIT", activity);
+    $.ajax({
+      url: '/getActivity',
+      method: 'POST',
+      data: activityData,
+      success: function(resp) {
+        var htmlForArticle = page.loadTemplate('#activityTmpl',resp);
+        $('.activity').prepend(htmlForArticle);
+        $('form > input[type="text"]').val('');
+      },
+      failure: function(resp) {
+        console.log("FAILURE", resp);
+      }
+    });
+  },
+
 
 };
