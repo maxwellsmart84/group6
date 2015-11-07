7bc950ed2eba58dad659645d7548d1b4c21807d2
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-11-06 20:55:19 -0500

    Aaron Seibert is the fucking shit and drunk and fixed my full screen background.

--- client/main.js
+++ client/main.js
@@ -16,7 +16,20 @@ var page= {
     page.videBackground();
 
   },
-
+  videBackground: function (){
+    $('#frontPage').vide('DreamBig.mp4');
+    $('#frontPage').vide('DreamBig.mp4', {
+        volume: 0,
+        playbackRate: .4,
+        muted: true,
+        loop: true,
+        autoplay: true,
+        position: '50% 50%', // Similar to the CSS `background-position` property.
+        posterType: 'png', // Poster image type. "detect" — auto-detection; "none" — no poster; "jpg", "png", "gif",... - extensions.
+        resizing: true, // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing
+        bgColor: 'transparent' // Allow custom background-color for Vide div
+    });
+  },
 
 
 };
