55a5bc4373c0589a2c22c2613f9fab0e7832a4f3
Lindsay Williams <williamsl25@yahoo.com>
2015-11-06 22:58:40 -0500

    js is working when I use heroku server.  It loads bucket items, edits them and deletes them.  I still am not able to get strikethrough to display

--- client/lindsay.js
+++ client/lindsay.js
@@ -1,73 +1,137 @@
-$(document).ready(function () {
-  page.init();
-});
-var page= {
+
+var page = {
+    url: "https://tiny-tiny.herokuapp.com/collections/bucketList",
   init:function(){
     page.initStyling();
     page.initEvents();
   },
   initStyling:function(){
-    page.getUser();
-    page.grabBucketFromServer();
+    page.getItem();
 
   },
   initEvents:function(){
 
-    },
-  getTemplate: function(name) {
-    return _.template(templates[name]);
-  },
-  loadTemplate: function(name, val) {
-      var tmpl = page.getTemplate(name);
-      return tmpl(val);
-  },
+  // EDIT Bucket Item-------------
+  $('section').on('click', '.listItem', function (event){
+  event.preventDefault();
+  $(this).closest('.listItem').replaceWith('<input type="text" class="updateListItem" placeholder="Edit Bucket List Item" name="updateListItem"</input>');
+  $('.updateListItem').parent().siblings('.editItem').addClass('show');
+  });
 
-    createNewBucket: function() {
-      var bucketText = $('form > input[name="bucket"]').val();
-      var newBucket = {
-        bucket: bucketText
-      };
-      // console.log("NEW bucket", newBucket)
+  $('.inputs').on('click', '#editedItem', function (event) {
+  event.preventDefault();
+  var itemId = $('.updateListItem').closest('article').data('itemid');
+  var editedListItem = {
+    title: $('.updateListItem').val(),
+    complete: false
+  }
+  page.updateItem(itemId, editedListItem);
 
-      page.sendBucketsToServer(newBucket);
-      // sendBucketsToServer
+  });
 
-    },
+  //CREATE NEW BUCKET ITEM------------
+  $('.createItem').on('submit', function(event){
+      event.preventDefault();
+        var newItem = {
+          title: $(this).find('input[name="newTitle"]').val(),
+          complete: false,
 
+        };
+      page.createItem(newItem);
+  });
 
+  //DELETE BUCKET//
+    $('section').on('click', '.deleteItem', function (event){
+      event.preventDefault();
+      var taskId = $(this).closest('article').data('itemid');
+      page.deleteItem(taskId);
+    });
 
-  grabBucketsFromServer: function() {
+    $('section').on('click', '.completeItem', function(event){
+      event.preventDefault();
+      $(this).parent().siblings('h3').toggleClass('complete')
+
+    });
+
+    //Delete All Items
+  $('#completeAll').on('click', function(event){
+    event.preventDefault();
+    $(this).parent().parent().siblings().children().children().children('h3').toggleClass('complete')
+    });
+
+},
+
+getItem: function() {
     $.ajax({
+      url: page.url,
       type: 'GET',
-      url: '/globalBucket',
-      success: function(data) {
-        console.log("SUCCESSSSSSSSSSSSSSSSSSS: ", data);
-        bucketData = JSON.parse(data);
-        console.log(JSON.parse(data))
-        // page.loadBucket(data);
+      success: function (bucket) {
+        var template = _.template(templates.bucket);
+        var markup = "";
+        bucket.forEach(function(item, idx, arr){
+          markup += template(item);
+        });
+        console.log('markup is...', markup);
+        $('section').html(markup);
+        var count = $('.listItem').length;
+        $('#itemCount').html(count);
+
+        //this is where I will add the completed tasks and amount of items left based on length
       },
-      failure: function(data) {
-        // console.log("FAILURE: ", data);
+      failure: function (err) {
+        console.log("DID NOT GET ITEM", err);
       }
     });
   },
-  // sendBucketsToServer: function(bucket) {
-  //   // console.log("IN TRANSIT", bucket);
-  //   $.ajax({
-  //     url: '/insertBucket',
-  //     method: 'POST',
-  //     data: bucketData,
-  //     success: function(resp) {
-  //       var htmlForArticle = page.loadTemplate('#bucketTmpl',resp);
-  //       $('h4').prepend(htmlForArticle);
-  //
-  //     },
-  //     failure: function(resp) {
-  //       // console.log("FAILURE", resp);
-  //     }
-  //   });
-  // },
 
+createItem: function(newItem) {
+  $.ajax({
+    url: page.url,
+    data: newItem,
+    type: 'POST',
+    success: function (data) {
+      console.log("SUCCESSFULLY CREATED NEW BUCKET", data);
+      page.getItem();
+    },
+    failure: function (err) {
+      console.log("DID NOT CREATE NEW BUCKET", err);
+    }
+  });
+  $('input').val('');
 
+  },
+  // console.log("NEW bucket", newBucket)
 
-};
+  deleteItem: function(itemId) {
+    $.ajax({
+      url: page.url + "/" + itemId,
+      type: 'DELETE',
+      success: function (data) {
+        console.log("Delete success!", data);
+        page.getItem();
+      },
+      failure: function (err) {
+        console.log("delete failed",err);
+      }
+    });
+  },
+  updateItem: function(itemId, editedItem) {
+    $.ajax({
+      url: page.url + "/" + itemId,
+      type: 'PUT',
+      data: editedItem,
+      success: function(data) {
+        console.log("update success!", data);
+        page.getItem();
+
+      },
+
+      failure: function(err) {
+        console.log("update failure", err);
+      }
+    });
+  }
+}
+$(document).ready(function () {
+   page.init();
+});
--- client/template.js
+++ client/template.js
@@ -1,14 +1,25 @@
+var templates = {};
 
-  var templates = {
+templates.bucket=[
+'<article data-itemid="<%= _id %>">',
+'<div class="bucketLineItem">',
+'<span>',
+'<a class="completeItem" href="#">',
+'<i class="fa fa-check fa-2x">',
+'</i>',
+'</a>',
+'</span>',
+'<h4 class="listItem"><%= title %></h4>',
+'<span>',
+'<a class="deleteItem" href="#">',
+'<i class="fa fa-trash-o fa-2x">',
+'</i>',
+'</a>',
+'</span>',
+'</div>',
+'<div class="editItem">',
+'<button id="editedItem" class="btn btn-primary">Edit Item</button>',
+'</div>',
+'</article>'
 
-    bucketTmpl: ['<article class="bucket" data-index="<%= _id %>">',
-               "<p><%= bucket %></p>",
-              ' <button class="delete">Delete</button>',
-              ' <button class="edit">Edit</button>',
-              '<section class="edit-field">',
-              '<input type="text" name="bucket" value="<%= bucket %>" />',
-              '<input type="submit" name="edit" class="nothing" />',
-              '</section></article>'
-              ].join("")
-
-  }
+].join("");
