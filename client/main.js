8e63a98f2d88f81d746c728a52a4176275241463
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-11-08 15:57:09 -0500

    fixed login box

--- client/main.js
+++ client/main.js
@@ -14,10 +14,9 @@ var page= {
   },
   initEvents:function(){
     page.loginEvent();
-    page.editItem();
-    page.deleteBucket();
-    page.createItemClick();
     page.videBackground();
+    page.newUserEvent();
+
 
   },
 
@@ -42,6 +41,7 @@ var page= {
     $("#frontPage").on("click", "#loginClick", function(event){
       event.preventDefault();
       $("#loginBox").removeClass("hidden-class");
+      $("#signUpBox").addClass("hidden-class")
     });
   },
 
@@ -49,104 +49,8 @@ var page= {
     $("#loginBox").on("click", "#signUp", function(event){
       event.preventDefault();
       console.log(event.target);
-      $("#inputFirstName").removeClass("hidden-class");
+      $("#loginBox").addClass("hidden-class");
+      $("#signUpBox").removeClass("hidden-class");
     });
-  },
-
-
-  editItem: function() {
-    $('section').on('click', '.listItem', function (event){
-      event.preventDefault();
-      $(this).closest('.listItem').replaceWith('<input type="text" class="updateListItem" placeholder="Edit Bucket List Item" name="updateListItem"</input>');
-      $('.updateListItem').parent().siblings('.editItem').addClass('show');
-    });
-
-    $('.inputs').on('click', '#editedItem', function (event) {
-      event.preventDefault();
-      var itemId = $('.updateListItem').closest('article').data('itemid');
-      var editedListItem = {
-        title: $('.updateListItem').val(),
-        complete: false
-      }
-      page.updateItem(itemId, editedListItem);
-
-    });
-  },
-
-  deleteBucket: function(){
-    $('section').on('click', '.deleteItem', function (event){
-      event.preventDefault();
-      var taskId = $(this).closest('article').data('itemid');
-      page.deleteItem(taskId);
-    });
-  //strikethrough when click the check
-    $('section').on('click', '.completeItem', function(event){
-      event.preventDefault();
-      $(this).parent().siblings('h4').toggleClass('complete')
-
-    });
-  },
-
-  createItemClick: function (){
-  $('.createItem').on('submit', function(event){
-      event.preventDefault();
-        var newItem = {
-          title: $(this).find('input[name="newTitle"]').val(),
-          complete: false,
-
-        };
-      page.createItem(newItem);
-    });
-  },
-
-
-  createItem: function(newItem) {
-    $.ajax({
-      url: "/",
-      data: newItem,
-      type: 'POST',
-      success: function (data) {
-        console.log("SUCCESSFULLY CREATED NEW BUCKET", data);
-        page.getItem();
-      },
-      failure: function (err) {
-        console.log("DID NOT CREATE NEW BUCKET", err);
-      }
-    });
-    $('input').val('');
-
-    },
-
-    deleteItem: function(itemId) {
-      $.ajax({
-        url: page.url + "/" + itemId,
-        type: 'DELETE',
-        success: function (data) {
-          console.log("Delete success!", data);
-          page.getItem();
-        },
-        failure: function (err) {
-          console.log("delete failed",err);
-        }
-      });
-    },
-
-
-    updateItem: function(itemId, editedItem) {
-      $.ajax({
-        url: page.url + "/" + itemId,
-        type: 'PUT',
-        data: editedItem,
-        success: function(data) {
-          console.log("update success!", data);
-          page.getItem();
-
-        },
-
-        failure: function(err) {
-          console.log("update failure", err);
-        }
-      });
-    },
-
-};
+  }
+}
